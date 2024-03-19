dd44720e2bb6386e0680e5c5ec5b5b6f36ece527
Matias Godoy <79279761+matigodoy@users.noreply.github.com>
2024-03-19 19:38:55 -0300

    Initial commit

---
+++ src/Infrastructure.Tests/FileBuilderTests.cs
@@ -0,0 +1,26 @@
+﻿using Core;
+using Xunit;
+
+namespace Infrastructure.Tests
+{
+    public class FileBuilderTests
+    {
+        private readonly FileBuilder<object> _fileBuilder;
+
+        public FileBuilderTests()
+        {
+            _fileBuilder = new FileBuilder<object>();
+        }
+
+        [Fact(Skip = "IronPDF now requires licensing, skipping this test for now")]
+        public void ToPdf_ReturnsByteArray_GivenHtmlString()
+        {
+            var htmlContent = "<html><body><p>Foo</p><p>Bar</p></body></html>";
+
+            var actual = _fileBuilder.ToPdf(htmlContent);
+
+            Assert.NotNull(actual);
+            Assert.True(actual.Length > 0);
+        }
+    }
+}
